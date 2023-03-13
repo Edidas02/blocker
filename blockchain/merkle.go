a554fd17510e400b7dcee5183f098bc11248eec7
Edward Xiong <53875786+Edidas02@users.noreply.github.com>
2023-03-13 01:28:06 -0500

    testing functionality

--- blockchain/merkle.go
+++ blockchain/merkle.go
@@ -1,32 +1,65 @@
 package blockchain
 
-import "github.com/dgraph-io/badger"
+import (
+	"crypto/sha256"
+	"log"
+)
 
-type BlockChainIterator struct {
-	CurrentHash []byte
-	Database    *badger.DB
+type MerkleTree struct {
+	RootNode *MerkleNode
 }
 
-func (chain *BlockChain) Iterator() *BlockChainIterator {
-	iter := &BlockChainIterator{chain.LastHash, chain.Database}
-
-	return iter
+type MerkleNode struct {
+	Left  *MerkleNode
+	Right *MerkleNode
+	Data  []byte
 }
 
-func (iter *BlockChainIterator) Next() *Block {
-	var block *Block
+func NewMerkleNode(left, right *MerkleNode, data []byte) *MerkleNode {
+	node := MerkleNode{}
 
-	err := iter.Database.View(func(txn *badger.Txn) error {
-		item, err := txn.Get(iter.CurrentHash)
-		Handle(err)
-		encodedBlock, err := item.Value()
-		block = Deserialize(encodedBlock)
+	if left == nil && right == nil {
+		hash := sha256.Sum256(data)
+		node.Data = hash[:]
+	} else {
+		prevHashes := append(left.Data, right.Data...)
+		hash := sha256.Sum256(prevHashes)
+		node.Data = hash[:]
+	}
 
-		return err
-	})
-	Handle(err)
+	node.Left = left
+	node.Right = right
 
-	iter.CurrentHash = block.PrevHash
+	return &node
+}
 
-	return block
+func NewMerkleTree(data [][]byte) *MerkleTree {
+	var nodes []MerkleNode
+
+	for _, dat := range data {
+		node := NewMerkleNode(nil, nil, dat)
+		nodes = append(nodes, *node)
+	}
+
+	if len(nodes) == 0 {
+		log.Panic("No merkel nodes")
+	}
+
+	for len(nodes) > 1 {
+		if len(nodes)%2 != 0 {
+			nodes = append(nodes, nodes[len(nodes)-1])
+		}
+
+		var level []MerkleNode
+		for i := 0; i < len(nodes); i += 2 {
+			node := NewMerkleNode(&nodes[i], &nodes[i+1], nil)
+			level = append(level, *node)
+		}
+
+		nodes = level
+	}
+
+	tree := MerkleTree{&nodes[0]}
+
+	return &tree
 }
