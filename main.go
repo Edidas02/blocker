3347c7433575a7a0bf62750c6a7f2f31d0c991ee
Edward Xiong <53875786+Edidas02@users.noreply.github.com>
2023-03-13 01:25:19 -0500

    updated main

--- main.go
+++ main.go
@@ -1,27 +1,14 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
-	"fmt"
-	"github.com/Edidas02/blocker/blockchain"
+	"os"
+
+	"github.com/tensor-programming/golang-blockchain/cli"
 )
 
-
 func main() {
-	chain := blockchain.InitBlockChain()
+	defer os.Exit(0)
 
-	chain.AddBlock("first block (not including genesis)")
-	chain.AddBlock("second block")
-	chain.AddBlock("third block")
-
-	for _, block := range chain.blocks {
-		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
-		fmt.Printf("Data in Block: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
-
-		pow := blockchain.NewProof(block)
-		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
-		fmt.Println()
-	}
+	cmd := cli.CommandLine{}
+	cmd.Run()
 }
